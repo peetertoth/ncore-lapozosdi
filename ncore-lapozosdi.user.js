0ca3e52e82ee406281eef86503f0b1d1ea884925
peetertoth <peeter.toth@gmail.com>
2017-01-25 21:32:19 +0100

    increase version number

--- ncore-lapozosdi.user.js
+++ ncore-lapozosdi.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Ncore lapozósdi
 // @namespace    peetft.ddns.net
-// @version      0.32
+// @version      0.33
 // @description  Ncore kiegészítő
 // @author       peeter.toth@gmail.com
 // @match        https://ncore.cc/torrents.php*
